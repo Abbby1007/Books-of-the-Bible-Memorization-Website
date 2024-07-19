f219bbf5dc58ee304255bc31b6e38916f242c0a9
Abbby1007 <Abigailloken25@gmail.com>
2024-07-19 23:30:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
-const buttonA = document.getElementById('choiceA');
+var click = 0;
 
-const buttonB = document.getElementById('choiceB');
-
-const buttonC = document.getElementById('choiceC');
-
-const buttonD = document.getElementById('choiceD');
-
-const display = document.getElementById("result");
-
-const rockButton = document.getElementById('ROCK');
-
-// rockButton.addEventListener('click', () => playRound('ROCK'));
-
-// buttonA.addEventListener('click', () => questionOne('choiceA'));
-
-// buttonB.addEventListener('click', () => questionOne('choiceB'));
-
-// buttonC.addEventListener('click', () => questionOne('choiceC'));
-
-// buttonD.addEventListener('click', () => questionOne('choiceD'));
-
-// function questionOne(playerChoice){
-//   if(playerChoice === 'choiceA'){
-//     buttonfunction();
-
-//   display.textContent = "HEllo World :DDDDD"
-//   }
-// }
-
-
-function buttonfunction() {
-  document.getElementById("button-appear").innerHTML = '<a href = "secondlevel.html"> <button> Move on </button> </a>'
+function restart(){
+  click ++;
+  if(click === 1){
+    document.getElementById("button-appear").innerHTML = '<a href = "index.html"> <button> Restart </button> </a>'
+  }  
 }
 
-function move(){
-  document.getElementById("button-appear").innerHTML = '<a href = "thirdlevel.html"> <button> Move on </button> </a>'
+
+
+function newQuestion1(){
+ click ++;
+  if(click === 1){
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion2.html"> <button> Move on </button> </a>'
+  }
+    
 }
+function newQuestion2(){
+ click ++;
+  if(click === 1){
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion3.html"> <button> Move on </button> </a>'
+  }
+
+}
+
