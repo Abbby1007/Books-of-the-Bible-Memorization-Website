f7cf6c25190b836945db4292b86cfa28b33c02af
Abbby1007 <Abigailloken25@gmail.com>
2024-07-25 03:16:59 +0900

    Adding more input options for page numbers for old testament books
    Adding more input options for page numbers for old testament books

---
+++ inputScript.js
@@ -0,0 +1,99 @@
+let genesisPage;
+let exodusPage;
+let leviticusPage;
+let numbersPage;
+let deuteronomyPage
+let joshuaPage
+let judgesPage
+let ruthPage
+let firstSamuelPage
+let secondSamuelPage
+let firstKingsPage
+function pageNumbers() {
+  // Genesis Page number start
+  genesisPage = document.getElementById("genesisPage").value;
+  console.log(`Genesis page Number: ${genesisPage}`)
+  // Genesis end
+
+  // Exodus page number start
+  exodusPage = document.getElementById("exodusPage").value;
+  console.log(`Exodus page Number: ${exodusPage}`)
+  // Exodus page number end
+
+  // Leviticus page number start
+  leviticusPage = document.getElementById("leviticusPage").value;
+  console.log(`Leviticus page Number: ${leviticusPage}`)
+  // Leviticus page number end
+
+  // Numbers page number start
+  numbersPage = document.getElementById("numbersPage").value;
+  console.log(`Numbers page Number: ${numbersPage}`)
+  // Numbers page number end
+
+  //  Deuteronomy page number start
+  deuteronomyPage = document.getElementById("deuteronomyPage").value;
+  console.log(`Deuteronomy page Number: ${deuteronomyPage}`)
+  //  Deuteronomy page number end
+
+  // // Joshua page number start
+  joshuaPage = document.getElementById("joshuaPage").value;
+  console.log(`Joshua page Number: ${joshuaPage}`)
+  // // Joshua page number end
+
+  //  Judges page number start
+  judgesPage = document.getElementById("judgesPage").value;
+  console.log(`Numbers page Number: ${judgesPage}`)
+  //  Judges page number end
+
+  //  Ruth page number start
+  ruthPage = document.getElementById("ruthPage").value;
+  console.log(`Ruth page Number: ${ruthPage}`)
+  //  Ruth page number end
+
+  //  1 Samuel page number start
+  firstSamuelPage = document.getElementById("1SamuelPage").value;
+  console.log(`1 Samuel page Number: ${firstSamuelPage}`)
+  //  1 Samuel page number end
+
+  //  2 Samuel page number start
+  secondSamuelPage = document.getElementById("2SamuelPage").value;
+  console.log(`2 Samuel page Number: ${secondSamuelPage}`)
+  //  2 Samuel page number end
+
+  //  1 Kings page number start
+  firstKingsPage = document.getElementById("1KingsPage").value;
+  console.log(`1 Kings page Number: ${firstKingsPage}`)
+  //  1 Kings page number end
+
+  //  2 Kings page number start
+  secondKingsPage = document.getElementById("2KingsPage").value;
+  console.log(`2 Kings page Number: ${secondKingsPage}`)
+  //  2 Kings page number end
+
+  //  1 Chronicles page number start
+  firstChroniclesPage = document.getElementById("1 ChroniclesPage").value;
+  console.log(`1 Chronicles page Number: ${firstChroniclesPage}`)
+  //  1 Chronicles page number end
+
+  //  2 Chronicles page number start
+  secondChroniclesPage = document.getElementById("2 ChroniclesPage").value;
+  console.log(`2 Chronicles page Number: ${secondChroniclesPage}`)
+  //  2 Chronicles page number end
+
+  //  Ezra page number start
+  ezraPage = document.getElementById("ezraPage").value;
+  console.log(`Ezra page Number: ${ezraPage}`)
+  //  Ezra page number end
+
+  //  Nehemiah page number start
+  nehemiahPage = document.getElementById("nehemiahPage").value;
+  console.log(`Nehemiah page Number: ${nehemiahPage}`)
+  //  Nehemiah page number end
+
+  //  Esther page number start
+   estherPage = document.getElementById("estherPage").value;
+  console.log(`Esther page Number: ${estherPage}`)
+  //   Esther page number end
+
+}
+
--- script.js
+++ script.js
@@ -1,333 +1,286 @@
 
 var click = 0;
 
-function restart(){
-  click ++;
-  if(click === 1){
+function restart() {
+  click++;
+  if (click === 1) {
     document.getElementById("button-appear").innerHTML = '<a href = "index.html"> <button> Restart </button> </a>'
-  }  
+  }
 }
 
 
 
-function newQuestion1(){
- click ++;
-  if(click === 1){
+function newQuestion1() {
+  click++;
+  if (click === 1) {
     document.getElementById("button-appear").innerHTML = '<a href = "newQuestion2.html"> <button> Move on </button> </a>'
   }
-    
+
 }
-function newQuestion2(){
- click ++;
-  if(click === 1){
+function newQuestion2() {
+  click++;
+  if (click === 1) {
     document.getElementById("button-appear").innerHTML = '<a href = "newQuestion3.html"> <button> Move on </button> </a>'
   }
 
 }
 
-function newQuestion3(){
- click ++;
-  if(click === 1){
+function newQuestion3() {
+  click++;
+  if (click === 1) {
     document.getElementById("button-appear").innerHTML = '<a href = "newQuestion4.html"> <button> Move on </button> </a>'
   }
 
 }
 
-function newQuestion4(){
-  click ++;
-  if(click === 1){
+function newQuestion4() {
+  click++;
+  if (click === 1) {
     document.getElementById("button-appear").innerHTML = '<a href = "newQuestion5.html"> <button> Move on </button> </a>'
-}
+  }
 }
 
-function newQuestion5(){
-  click ++;
-  if(click === 1){
+function newQuestion5() {
+  click++;
+  if (click === 1) {
     document.getElementById("button-appear").innerHTML = '<a href = "newQuestion6.html"> <button> Move on </button> </a>'
-}
+  }
 }
 
-function newQuestion6(){
-  click ++;
-  if(click === 1){
+function newQuestion6() {
+  click++;
+  if (click === 1) {
     document.getElementById("button-appear").innerHTML = '<a href = "newQuestion7.html"> <button> Move on </button> </a>'
-}
+  }
 }
 
-function newQuestion7(){
-  click ++;
-  if(click === 1){
+function newQuestion7() {
+  click++;
+  if (click === 1) {
     document.getElementById("button-appear").innerHTML = '<a href = "newQuestion8.html"> <button> Move on </button> </a>'
-}
+  }
 }
 
-function newQuestion8(){
-  click ++;
-  if(click === 1){
+function newQuestion8() {
+  click++;
+  if (click === 1) {
     document.getElementById("button-appear").innerHTML = '<a href = "newQuestion9.html"> <button> Move on </button> </a>'
+  }
 }
-}
-function newQuestion9(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion10.html"> <button> Move on </button> </a>'
+function newQuestion9() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion10.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion10(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion11.html"> <button> Move on </button> </a>'
+function newQuestion10() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion11.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion11(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion12.html"> <button> Move on </button> </a>'
+function newQuestion11() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion12.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion12(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion13.html"> <button> Move on </button> </a>'
+function newQuestion12() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion13.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion13(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion14.html"> <button> Move on </button> </a>'
+function newQuestion13() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion14.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion14(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion15.html"> <button> Move on </button> </a>'
+function newQuestion14() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion15.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion15(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion16.html"> <button> Move on </button> </a>'
+function newQuestion15() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion16.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion16(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion17.html"> <button> Move on </button> </a>'
+function newQuestion16() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion17.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion17(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion18.html"> <button> Move on </button> </a>'
+function newQuestion17() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion18.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion18(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion19.html"> <button> Move on </button> </a>'
+function newQuestion18() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion19.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion19(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion20.html"> <button> Move on </button> </a>'
+function newQuestion19() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion20.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion20(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion21.html"> <button> Move on </button> </a>'
+function newQuestion20() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion21.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion21(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion22.html"> <button> Move on </button> </a>'
+function newQuestion21() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion22.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion22(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion23.html"> <button> Move on </button> </a>'
+function newQuestion22() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion23.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion23(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion24.html"> <button> Move on </button> </a>'
+function newQuestion23() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion24.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion24(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion25.html"> <button> Move on </button> </a>'
+function newQuestion24() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion25.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion25(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion26.html"> <button> Move on </button> </a>'
+function newQuestion25() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion26.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion26(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion27.html"> <button> Move on </button> </a>'
+function newQuestion26() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion27.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion27(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion28.html"> <button> Move on </button> </a>'
+function newQuestion27() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion28.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion28(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion29.html"> <button> Move on </button> </a>'
+function newQuestion28() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion29.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion29(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion30.html"> <button> Move on </button> </a>'
+function newQuestion29() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion30.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion30(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion31.html"> <button> Move on </button> </a>'
+function newQuestion30() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion31.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion31(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion32.html"> <button> Move on </button> </a>'
+function newQuestion31() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion32.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion32(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion33.html"> <button> Move on </button> </a>'
+function newQuestion32() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion33.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion33(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion34.html"> <button> Move on </button> </a>'
+function newQuestion33() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion34.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion34(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion35.html"> <button> Move on </button> </a>'
+function newQuestion34() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion35.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion35(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion36.html"> <button> Move on </button> </a>'
+function newQuestion35() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion36.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion36(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion37.html"> <button> Move on </button> </a>'
+function newQuestion36() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion37.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion37(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion38.html"> <button> Move on </button> </a>'
+function newQuestion37() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion38.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion38(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "newQuestion39.html"> <button> Move on </button> </a>'
+function newQuestion38() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "newQuestion39.html"> <button> Move on </button> </a>'
   }
 }
 
-function newQuestion39(){
-  click ++;
-    if(click === 1){
-      document.getElementById("button-appear").innerHTML = '<a href = "congratulations.html"> <button> Move on </button> </a>'
+function newQuestion39() {
+  click++;
+  if (click === 1) {
+    document.getElementById("button-appear").innerHTML = '<a href = "congratulations.html"> <button> Move on </button> </a>'
   }
-}
-
-let username;
-let finalMessage = "";
-let currentRound = 1;
-const totalRound = 3; // total rounds can change with each level
-// document.getElementById("mySubmit").onclick = function(){
- 
-// }
-const display = document.getElementById('appear');
-const roundDisplay = document.getElementById('round');
-
-
-let genesisPage;
-let exodusPage;
-function pageNumbers (){
-  // Genesis Page number start
-  genesisPage = document.getElementById("genesisPage").value;
-console.log(`Genesis page Number: ${genesisPage}`)
-  // Genesis end
-
-  // Exodus page number start
-  exodusPage = document.getElementById("exodusPage").value;
-  console.log(`Exodus page Number: ${exodusPage}`)
-  // Exodus page number end
-
-  // Leviticus page number start
-  leviticusPage = document.getElementById("leviticusPage").value;
-  console.log(`Leviticus page Number: ${leviticusPage}`)
-  // Leviticus page number end
-
-  // Numbers page number start
-  numbersPage = document.getElementById("numbersPage").value;
-  console.log(`Numbers page Number: ${numbersPage}`)
-  // Numbers page number end
-
-  //  Deuteronomy page number start
-  deuteronomyPage = document.getElementById(" deuteronomyPage").value;
-  console.log(`Numbers page Number: ${deuteronomyPage}`)
-  //  Deuteronomy page number end
-
-  // Joshua page number start
-  joshuaPage = document.getElementById("joshuaPage").value;
-  console.log(`Joshua page Number: ${joshuaPage}`)
-  // Joshua page number end
- 
-
 }
 
