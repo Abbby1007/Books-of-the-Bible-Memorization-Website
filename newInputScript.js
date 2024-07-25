83d0f84e098127c72b8a8bfde47e98c1cdbe6263
Abbby1007 <Abigailloken25@gmail.com>
2024-07-26 07:57:17 +0900

    Page Input Data for the New Testament Books
    Page Input Data for the New Testament Books

---
+++ newInputScript.js
@@ -0,0 +1,138 @@
+function hello(){
+  // Matthew Page number start
+    let matthewPage = document.getElementById("matthewPage").value;
+    console.log(`Matthew page Number: ${matthewPage}`)
+    // Matthew end
+
+    // Mark page number start
+    let markPage = document.getElementById("markPage").value;
+    console.log(`Mark page Number: ${markPage}`)
+    // Mark page number end
+
+    // Luke page number start
+    let lukePage = document.getElementById("lukePage").value;
+    console.log(`Luke page Number: ${lukePage}`)
+    // Luke page number end
+
+    // John page number start
+    let johnPage = document.getElementById("johnPage").value;
+    console.log(`John page Number: ${johnPage}`)
+    // John page number end
+
+    //  Acts page number start
+    let actsPage = document.getElementById("actsPage").value;
+    console.log(`Acts page Number: ${actsPage}`)
+    //  Acts page number end
+
+    // Romans page number start
+    let romansPage = document.getElementById("romansPage").value;
+    console.log(`Romans page Number: ${romansPage}`)
+    // Romans page number end
+
+    //  1Corinthians page number start
+    let firstCorinthiansPage = document.getElementById("1CorinthiansPage").value;
+    console.log(`1 Corinthians page Number: ${firstCorinthiansPage}`)
+    //  1Corinthians page number end
+
+    //  2Corinthians page number start
+    let secondCorinthiansPage = document.getElementById("2CorinthiansPage").value;
+    console.log(`2 Corinthians page Number: ${secondCorinthiansPage}`)
+    //  2Corinthians page number end
+
+    //  Galations page number start
+    let galationsPage = document.getElementById("galationsPage").value;
+    console.log(`Galations page Number: ${galationsPage}`)
+    //  Galations page number end
+
+    //  Ephesians page number start
+    let ephesiansPage = document.getElementById("ephesiansPage").value;
+    console.log(`Ephesians page Number: ${ephesiansPage}`)
+    //  Ephesians page number end
+
+    //  Philippians page number start
+    let philippiansPage = document.getElementById("philippiansPage").value;
+    console.log(`Philippians page Number: ${philippiansPage}`)
+    //  Philippians page number end
+
+    //  Colossians page number start
+    let colossiansPage = document.getElementById("colossiansPage").value;
+    console.log(`Colossians page Number: ${colossiansPage}`)
+    //  Colossians page number end
+
+    // 1Thessalonians page number start
+    let firstThessaloniansPage = document.getElementById("1ThessaloniansPage").value;
+    console.log(`1 Thessalonians page Number: ${firstThessaloniansPage}`)
+    // 1Thessalonians page number end
+
+    //  2Thessalonians page number start
+    let secondThessaloniansPage = document.getElementById("2ThessaloniansPage").value;
+    console.log(`2 Thessalonians page Number: ${secondThessaloniansPage}`)
+    //  2Thessalonians page number end
+
+    // 1Timothy page number start
+    let firstTimothyPage = document.getElementById("1TimothyPage").value;
+    console.log(`1 Timothy page Number: ${firstTimothyPage}`)
+    //  1Timothy page number end
+
+    //  2Timothy page number start
+    let secondTimothyPage = document.getElementById("2TimothyPage").value;
+    console.log(`2 Timothy page Number: ${secondTimothyPage}`)
+    //  2Timothy page number end
+
+    //  Titus page number start
+    let titusPage = document.getElementById("titusPage").value;
+    console.log(`Titus page Number: ${titusPage}`)
+    //   Titus page number end
+
+    //  Philemon page number start
+    let philemonPage = document.getElementById("philemonPage").value;
+    console.log(`Philemon page Number: ${philemonPage}`)
+    //   Philemon page number end
+
+    //  Hebrews page number start
+    let hebrewsPage = document.getElementById("hebrewsPage").value;
+    console.log(`Hebrews page Number: ${hebrewsPage}`)
+    // Hebrews page number end
+
+    let  // James page number start
+      jamesPage = document.getElementById("jamesPage").value;
+    console.log(`James page Number: ${jamesPage}`)
+    //   James page number end
+
+    //  1Peter page number start
+    let firstPeterPage = document.getElementById("1PeterPage").value;
+    console.log(`1 Peter page Number: ${firstPeterPage}`)
+    //   1Peter page number end
+
+    // 2Peter page number start
+    let secondPeterPage = document.getElementById("2PeterPage").value;
+    console.log(`2 Peter page Number: ${secondPeterPage}`)
+    // 2Peter end
+
+    //  1John page number start
+    let firstJohnPage = document.getElementById("1JohnPage").value;
+    console.log(`1 John page Number: ${firstJohnPage}`)
+    //  1John page number end
+
+    //  2John page number start
+    let secondJohnPage = document.getElementById("2JohnPage").value;
+    console.log(`2 John page Number: ${secondJohnPage}`)
+    //  2John page number end
+
+    //  3John page number start
+    let thirdJohnPage = document.getElementById("3JohnPage").value;
+    console.log(`3 John page Number: ${thirdJohnPage}`)
+    //  3John page number end
+
+    //  Jude page number start
+    let judePage = document.getElementById("judePage").value;
+    console.log(`Jude page Number: ${judePage}`)
+    //  Jude page number end
+
+    //  Revelation page number start
+    let revelationPage = document.getElementById("revelationPage").value;
+    console.log(`Revelation page Number: ${revelationPage}`)
+    //  Revelation page number end
+
+   document.getElementById("appear").innerHTML = '<a href = "newPageQuestions.html"> <button> To Quiz </button> </a>'
+}
